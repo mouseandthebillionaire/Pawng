c82dc4769eb8a9b2afa1f33ba7224923487bbc84
M Bethancourt <matt@mouseandthebillionaire.com>
2020-08-30 18:14:32 -0600

    Initial state of Pawng project

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+    private float     yPos;
+    public float      paddleSpeed = .05f;
+    public float      topWall, bottomWall;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            if (yPos > bottomWall) {
+                yPos -= paddleSpeed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            if (yPos < topWall) {
+                yPos += paddleSpeed;
+            }
+        }
+
+        transform.localPosition = new Vector3(transform.position.x, yPos, 0);
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# Pawng
+Pawng project for week 2 of ATLS 4(5)140 Game Development
+
+
