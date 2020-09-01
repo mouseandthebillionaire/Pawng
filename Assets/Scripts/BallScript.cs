28b34bb48a7cefc54aaf8cbc69a6f09777068dab
mouseandthebillionaire <matt@mouseandthebillionaire.com>
2020-09-01 09:12:10 -0600

    End of Day 1 - ball movement, two paddles, bounds, and score

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BallScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float ballSpeed;
+    public float maxSpeed = 10f;
+    public float minSpeed = 1f;
+    
+    private int[] dirOptions = {-1, 1};
+    private int hDir, vDir;
+    
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("Launch"); // StartCoroutine(Launch());
+    }
+
+
+    // Start the Ball Moving
+    public IEnumerator Launch() {
+        yield return new WaitForSeconds(1.5f);
+        
+        // Figure out directions
+        hDir = dirOptions[Random.Range(0, dirOptions.Length)];
+        vDir = dirOptions[Random.Range(0, dirOptions.Length)];
+        
+        // Add a horizontal force
+        rb.AddForce(transform.right * ballSpeed * hDir); // 1,0
+        // Add a vertical force
+        rb.AddForce(transform.up * ballSpeed * vDir); // 0,1
+    }
+
+    private void Reset()
+    {
+        transform.position = new Vector2(0, 0);
+        rb.velocity = Vector2.zero;
+        StartCoroutine("Launch");
+    }
+    
+    // if the ball goes out of bounds
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // Prevent ball from going too fast
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed) rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        if (Mathf.Abs(rb.velocity.y) > maxSpeed) rb.velocity = new Vector2(rb.velocity.x, maxSpeed);
+
+        // if wall
+        if (other.gameObject.tag == "LeftBounds")
+        {
+            // Point for the right
+            ScoreScript.S.UpdateScore(1);
+            // Reset the Ball
+            Reset();
+        }
+        if (other.gameObject.tag == "RightBounds")
+        {
+            // Point for the left
+            ScoreScript.S.UpdateScore(0);
+            // Reset the Ball
+            Reset();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public int leftScore, rightScore;
+    
+    public static ScoreScript S;
+
+    void Awake() {
+        S = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore(int scorer)
+    {
+        if (scorer == 0) leftScore++; 
+        if (scorer == 1) rightScore++;
+        
+        Debug.Log(leftScore + " : " + rightScore);
+    }
+
+    public void ResetScore()
+    {
+        leftScore = 0;
+        rightScore = 0;
+        
+    }
+}
